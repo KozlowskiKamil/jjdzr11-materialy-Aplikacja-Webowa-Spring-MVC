dde1c4ff9487acfc86c4d8e5652925480d6a5392
wojciechbulawa <bulawa.wojciech.firma@gmail.com>
2021-11-26 20:21:50 +0100

    Add thymeleaf resolver

---
+++ src/main/java/com/example/web/config/ThymeleafTemplateResolverConfig.java
@@ -0,0 +1,24 @@
+package com.example.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class ThymeleafTemplateResolverConfig {
+
+    @Bean
+    @Description("Thymeleaf template resolver serving HTML 5")
+    public ClassLoaderTemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+
+        templateResolver.setPrefix("templates2/");
+        templateResolver.setCacheable(false);
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        return templateResolver;
+    }
+}
